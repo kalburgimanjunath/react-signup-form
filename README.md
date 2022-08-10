de89ba7667a35588a1a752e17d17ecd6722e9768
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-10 17:18:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-emqvzf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-emqvzf)
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="main">
+      <div className="container">
+        <div>
+          <img
+            className="image"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9ujTCDgGCAjlEMlNDG9pKxC4S2g4bULE3zDVU20pELA&s"
+          />
+          <h1>Lorem ipsum</h1>
+          <p>
+            Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat
+            nisl et tempus et dolor tempus, sed et lorem adipiscing.
+          </p>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email address"
+            style={{ color: '#f0f0f0' }}
+          />
+          <button type="submit">Sign Up</button>
+        </div>
+        {/* social icons */}
+      </div>
+    </div>
+  );
+}
